1d9b63a5041d23d56a687ea10375bf35d6b0738e
Seungkuk <Seungkuk@users.noreply.github.com>
2023-01-16 07:25:03 -0500

    2 files modified, router was fixed by adding '/product' and product.id

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 import { Product, products } from '../products';
 
 @Component({
@@ -11,14 +12,15 @@ export class ProductDetailsComponent implements OnInit {
   
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    
     // First get the product id from the current route.
-    const routeParams  = this.route.snapshot.paramMap;
+    const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
 
-    // Find the product that correspond with the id provided in route.
-    this.product = products.find(product => product.id == productIdFromRoute);
+  // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 }
