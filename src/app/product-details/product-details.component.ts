14296e0915a416a7aa87ff7ed2dc53d7a8778e2b
Seungkuk <Seungkuk@users.noreply.github.com>
2023-01-17 06:39:35 -0500

    2 files modified, 2 added, car-service, button buy in product-details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 import { Product, products } from '../products';
 
@@ -12,7 +13,7 @@ export class ProductDetailsComponent implements OnInit {
   
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit() {
     
@@ -22,5 +23,11 @@ export class ProductDetailsComponent implements OnInit {
 
   // Find the product that correspond with the id provided in route.
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added now! " + this.cartService.getItems().length);
   }
 }
