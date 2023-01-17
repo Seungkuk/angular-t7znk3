317d73f3dcec38644acf3e5846eaf611d49085cf
Seungkuk <Seungkuk@users.noreply.github.com>
2023-01-17 07:04:18 -0500

    2 files modified, cart component to display all items

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,6 +8,7 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   
-  // items = this.cartService.getItems();
-  constructor(cartService: CartService) {}
+  items = this.cartService.getItems();
+  
+  constructor(private cartService: CartService) {}
 }
